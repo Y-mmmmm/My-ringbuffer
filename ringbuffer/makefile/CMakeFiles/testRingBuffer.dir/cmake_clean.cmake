936c6368a72ea68b5110103be6bb46b026c606e2
Yin Ming <1131594132@qq.com>
2020-07-03 10:25:03 +0800

    Add the source file and test file

---
+++ ringbuffer/makefile/CMakeFiles/testRingBuffer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testRingBuffer.dir/ringbuffer.c.o"
+  "CMakeFiles/testRingBuffer.dir/test.c.o"
+  "testRingBuffer.pdb"
+  "testRingBuffer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testRingBuffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
